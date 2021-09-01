399794cf9b105b03d75555ecd806a319ffa684d6
area1110 <khanhhuy1110@gmail.com>
2021-09-01 15:51:54 +0700

    improve dry

--- CrossFadeSlidesShow/scripts.js
+++ CrossFadeSlidesShow/scripts.js
@@ -12,27 +12,7 @@
   ];
   let currentIndexImg = 0;
 
-  // previous.onclick = changePre;
-  // next.onclick =  changeNext;
-
-  // function changeNext(){
-  //     if(currentIndexImg<imgList.length-1){
-  //         myImg.src = pathImg + imgList[++currentIndexImg];
-  //     }
-  // }
-  // function changePre(){
-  //     if(currentIndexImg>0){
-  //         myImg.src = pathImg + imgList[--currentIndexImg];
-  //     }
-  // }
-
-  previous.addEventListener("click", function (event) {
-    event.preventDefault();
-    currentIndexImg--;
-    if (currentIndexImg < 0) {
-      currentIndexImg = imgList.length - 1;
-    }
-    
+  function swapImg() {
     const myImg = document.createElement("img");
     myImg.src = pathImg + imgList[currentIndexImg];
     myImg.className = "fadeinimg";
@@ -41,6 +21,16 @@
     if (Img.length > 2) {
       content.removeChild(Img[0]);
     }
+  }
+
+  previous.addEventListener("click", function (event) {
+    event.preventDefault();
+    currentIndexImg--;
+    if (currentIndexImg < 0) {
+      currentIndexImg = imgList.length - 1;
+    }
+
+    swapImg();
   });
 
   next.addEventListener("click", function (event) {
@@ -49,14 +39,6 @@
     if (currentIndexImg === imgList.length) {
       currentIndexImg = 0;
     }
-    
-    const myImg = document.createElement("img");
-    myImg.src = pathImg + imgList[currentIndexImg];
-    myImg.className = "fadeinimg";
-    content.appendChild(myImg);
-    const Img = document.querySelectorAll("img");
-    if (Img.length > 2) {
-      content.removeChild(Img[0]);
-    }
+    swapImg();
   });
 })();
