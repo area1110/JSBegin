6b9a6fdb8397015393592fd6f9b05cb2407360a6
area1110 <khanhhuy1110@gmail.com>
2021-09-10 17:48:19 +0700

    public

--- hiragana/scripts.js
+++ hiragana/scripts.js
@@ -1,25 +1,20 @@
-(function () {
-  "use strict";
-  const hiraChar = ["あ", "い", "う", "え", "お"];
+const charArray = [
+  "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん",
+  "アイウウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン",
+];
 
-  function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getString(choice) {
+  let numChar = getRndInteger(1, 4);
+  let textRandom =
+    charArray[choice][getRndInteger(choice, charArray[choice].length - 1)];
+  for (let i = 0; i < numChar; i++) {
+    textRandom +=
+      charArray[choice][getRndInteger(choice, charArray[choice].length - 1)];
   }
-
-  document
-    .getElementsByTagName("button")[0]
-    .addEventListener("click", getString());
-
-  function getString() {
-    // event.preventDefault();
-    alert("test");
-    let numChar = getRndInteger(0, 4);
-    let textRandom;
-    for (let i = 0; i < numChar; i++) {
-      textRandom += hiraChar[getRndInteger(0, hiraChar.length - 1)];
-    }
-    document.getElementById("displayHira").innerHTML = textRandom;
-  }
-
-  
-})();
+  document.getElementById("display").innerHTML = textRandom;
+  return 1;
+}
