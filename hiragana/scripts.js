e46331b2610ab8434484c09d1bab22ace371998c
area1110 <khanhhuy1110@gmail.com>
2021-09-11 20:53:37 +0700

    add get romanji feature

--- hiragana/scripts.js
+++ hiragana/scripts.js
@@ -4,8 +4,55 @@
     "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン",
   ];
 
-  const romanArray = ["a", "i", "", "", ""];
-
+  const romanArray = [
+    "a",
+    "i",
+    "u",
+    "e",
+    "o",
+    "ka",
+    "ki",
+    "ku",
+    "ke",
+    "ko",
+    "sa",
+    "shi",
+    "su",
+    "se",
+    "so",
+    "ta",
+    "chi",
+    "tsu",
+    "te",
+    "to",
+    "na",
+    "ni",
+    "nu",
+    "ne",
+    "no",
+    "ha",
+    "hi",
+    "hu",
+    "he",
+    "ho",
+    "ma",
+    "mi",
+    "mu",
+    "me",
+    "mo",
+    "ya",
+    "yu",
+    "yo",
+    "ra",
+    "ri",
+    "ru",
+    "re",
+    "ro",
+    "wa",
+    "wo",
+    "n",
+  ];
+  const display = document.getElementById("display");
   let max, numChar, textRandom;
 
   function getRndInteger(min, max) {
@@ -21,16 +68,32 @@
       numChar = getRndInteger(1, max);
       textRandom =
         hiraKataString[choice][
-          getRndInteger(choice, hiraKataString[choice].length - 1)
+          getRndInteger(0, hiraKataString[choice].length - 1)
         ];
       for (let i = 0; i < numChar - 1; i++) {
         textRandom +=
           hiraKataString[choice][
-            getRndInteger(choice, hiraKataString[choice].length - 1)
+            getRndInteger(0, hiraKataString[choice].length - 1)
           ];
       }
     }
-    document.getElementById("display").innerHTML = textRandom;
+    display.innerHTML = textRandom;
+  }
+
+  function getRoman() {
+    max = document.getElementById("max").value;
+    max = Number(max);
+    if (!max) {
+      textRandom = "Vui lòng nhập số vào ô dưới";
+    } else {
+      textRandom = romanArray[getRndInteger(0, romanArray.length - 1)];
+      numChar = getRndInteger(1, max);
+      for (let i = 0; i < numChar - 1; i++) {
+        textRandom += " " + romanArray[getRndInteger(0, romanArray.length - 1)];
+      }
+    }
+    display.style.textTransform = "uppercase";
+    display.innerHTML = textRandom;
   }
 
   document.getElementById("gethira").addEventListener("click", function () {
@@ -40,6 +103,6 @@
     getString(1);
   });
   document.getElementById("getroma").addEventListener("click", function () {
-    getString(0);
+    getRoman();
   });
 })();
