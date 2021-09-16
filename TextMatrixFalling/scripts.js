97fa257fdb5c2e926346a7eb85a8c6012f3b26c2
area1110 <khanhhuy1110@gmail.com>
2021-09-16 22:12:16 +0700

    improve view

--- TextMatrixFalling/scripts.js
+++ TextMatrixFalling/scripts.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', eventWindowLoaded, false);
 
 function eventWindowLoaded () {
-  canvasApp(); //包含整个Canvas应用程序
+  canvasApp();
 }
+
 function canvasSupport (e) {
   
     return !!e.getContext;
@@ -16,21 +17,21 @@ function canvasApp () {
   }
   
   var ctx = canvas.getContext('2d');
-  var w = canvas.width = window.innerWidth - 10;
-  var h = canvas.height = window.innerHeight - 20;
+  var w = canvas.width = window.innerWidth;
+  var h = canvas.height = window.innerHeight;
 
   var yPositions = Array(300).join(0).split('');
   
   function runMatrix() {
     if (typeof Game_Interval != 'undefined') clearInterval(Game_interval);
-    Game_Interval = setInterval(drawScreen, 33);
+    Game_Interval = setInterval(drawScreen, 30);
   }
 
   function drawScreen () {
     ctx.fillStyle = 'rgba(0,0,0,.05)';
     ctx.fillRect(0, 0, w, h);
     ctx.fillStyle = '#0f0';
-    ctx.font = '15px Arial';
+    ctx.font = '17px Arial';
     yPositions.map(function(y, index){
       text = String.fromCharCode(1e2 + Math.random() * 33);
       x = (index * 10) + 10;
