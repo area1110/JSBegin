3a34b7d1745ac06163df544f93fa2e6ab834738f
area1110 <khanhhuy1110@gmail.com>
2021-09-19 11:09:31 +0700

    rau den

---
+++ em-thich-an-rau-den-ko/scripts.js
@@ -0,0 +1,73 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+(function () {
+  const WIDTH = 100;
+  const HEIGHT = 50;
+
+  function main() {
+    const FILE_PATH = "100x50-loan.txt";
+    readingFile(FILE_PATH).then(function (imgString) {
+      canvasApp(imgString);
+    });
+  }
+
+  async function readingFile(filePath) {
+    const r = await fetch(filePath);
+    const t = await r.text();
+    return Promise.resolve(t);
+  }
+
+  function canvasSupport(e) {
+    return !!e.getContext;
+  }
+
+  function canvasApp(stringImg) {
+    let myCanvas = document.getElementById("myCanvas");
+
+    if (!canvasSupport(myCanvas)) {
+      return;
+    }
+
+    myCanvas.width = window.innerWidth;
+    myCanvas.height = window.innerHeight;
+    let ctx = myCanvas.getContext("2d");
+
+    arrayCharImg = Array.from(
+      stringImg.replaceAll("\r", "").replaceAll("\n", "")
+    );
+    arrayBoolean = Array(arrayCharImg.length).fill(false, 0); //initial the boolean array for showing the visible of each char on canvas
+    //the x position of one char
+
+    function runMatrix() {
+      if (typeof Game_Interval != "undefined") clearInterval(Game_interval);
+      Game_Interval = setInterval(drawTheScreen, 10);
+    }
+
+    function drawTheScreen() {
+      ctx.fillStyle = "black";
+      ctx.font = "10px monospace";
+      arrayCharImg.map(function (y, index) {
+        random1 = getRandomInt(arrayCharImg.length);
+        random2 = getRandomInt(arrayCharImg.length);
+        random3 = getRandomInt(arrayCharImg.length);
+        if (
+          !arrayBoolean[index] &&
+          (index === random1 || index === random2 || index === random3)
+        ) {
+          col = Math.ceil((index + 1) / WIDTH) - 1;
+          x = (index - WIDTH * col) * 6 + window.innerWidth * 0.1;
+          y = col * 13 + 20;
+          ctx.fillText(arrayCharImg[index], x, y);
+          arrayBoolean[index] = true;
+        }
+      });
+    }
+
+    runMatrix();
+  }
+
+  main();
+  window.addEventListener("resize", main);
+})();
--- fap_scripts.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    'use strict';
-    const location = document.querySelector("select#ctl00_mainContent_ddlCampus");
-    location.value = '3';
-    const loginButton = document.querySelector("#loginform .row .col-sm-4 .abcRioButtonContentWrapper");
-    window.setTimeout(function(){loginButton.click();}, 3000);
-})();
