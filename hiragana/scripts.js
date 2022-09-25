a8513aea4d01c3d35ffb8f1fc40191c0fd89a985
area1110 <khanhhuy1110@gmail.com>
2022-09-25 12:33:08 +0700

    Update tool with copy and max min

--- hiragana/scripts.js
+++ hiragana/scripts.js
@@ -60,12 +60,16 @@
   }
 
   function getString(choice) {
+    min = document.getElementById("min").value;
     max = document.getElementById("max").value;
     max = Number(max);
-    if (!max) {
+    min = Number(min);
+    if (!min) {
+      textRandom = "Vui lòng nhập số vào ô dưới";
+    } else if (!max) {
       textRandom = "Vui lòng nhập số vào ô dưới";
     } else {
-      numChar = getRndInteger(1, max);
+      numChar = getRndInteger(min, max);
       textRandom =
         hiraKataString[choice][
           getRndInteger(0, hiraKataString[choice].length - 1)
@@ -81,28 +85,81 @@
   }
 
   function getRoman() {
+    min = document.getElementById("min").value;
     max = document.getElementById("max").value;
     max = Number(max);
-    if (!max) {
+    min = Number(min);
+    if (!min) {
+      textRandom = "Vui lòng nhập số vào ô dưới";
+    } else if (!max) {
       textRandom = "Vui lòng nhập số vào ô dưới";
     } else {
       textRandom = romanArray[getRndInteger(0, romanArray.length - 1)];
-      numChar = getRndInteger(1, max);
+      numChar = getRndInteger(min, max);
       for (let i = 0; i < numChar - 1; i++) {
         textRandom += " " + romanArray[getRndInteger(0, romanArray.length - 1)];
       }
     }
-    display.style.textTransform = "uppercase";
+    // display.style.textTransform = "uppercase";
     display.innerHTML = textRandom;
   }
 
+  var btnCopy = document.getElementById("copy");
+
+  // Auto copy result after generate text successful
   document.getElementById("gethira").addEventListener("click", function () {
     getString(0);
+    copyTextToClipboard(textRandom);
   });
   document.getElementById("getkata").addEventListener("click", function () {
     getString(1);
+    copyTextToClipboard(textRandom);
   });
   document.getElementById("getroma").addEventListener("click", function () {
     getRoman();
+    copyTextToClipboard(textRandom);
+  });
+
+  btnCopy.addEventListener("click", function () {
+    copyTextToClipboard(textRandom);
   });
 })();
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+}
